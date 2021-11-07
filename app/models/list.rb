9b9fb756f9a4ce8e9f37d9b81cac3737a99839f7
Artur Veselovski <arveselovski@gmail.com>
2021-11-07 15:29:46 +0200

    added acts_as_list list scope to board

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class List < ApplicationRecord
-  acts_as_list
+  acts_as_list scope: :board
 
   belongs_to :board
   has_many :cards, -> { order(position: :asc) }, dependent: :destroy
